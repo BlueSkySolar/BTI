f4a7cdc6699160e5605edc18ec2ef8674893bebb
Jerry Li <jerryli97@outlook.com>
2016-04-24 00:34:27 -0400

    testing plotting functionality

--- main.py
+++ main.py
@@ -1,61 +1,35 @@
 '''
 The GUI
 
-Just some test PyQt code for now
+Currently in the progress of testing plotting with PyQtGraph
 '''
 
 import sys
 from PyQt4 import QtGui, QtCore
+from btiplottest_ui import Ui_MainWindow
+import bti
 
-
-class Example(QtGui.QWidget):
-
-    def __init__(self):
-        super(Example, self).__init__()
-
-        self.initUI()
-
-    def initUI(self):
-
-        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
-
-        self.setToolTip('This is a <b>QWidget</b> widget')
-        btn = QtGui.QPushButton('Button', self)
-        btn.setToolTip('Press this button to <b>quit</b> the window')
-        btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
-
-        btn.resize(btn.sizeHint())
-        btn.move(50, 50)
-        self.resize(500, 400)
-        self.center()
-
-        self.setWindowTitle('BTI')
-
-        self.show()
-
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QtGui.QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
-    def closeEvent(self, event):
-
-        reply = QtGui.QMessageBox.question(self, 'Message',
-                    "Are you sure to quit?", QtGui.QMessageBox.Yes |
-                    QtGui.QMessageBox.No, QtGui.QMessageBox.No)
-
-        if reply == QtGui.QMessageBox.Yes:
-            event.accept()
-        else:
-            event.ignore()
 
 
 def main():
 
     app = QtGui.QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+
+    win = QtGui.QMainWindow()
+    # win.setWindowTitle('pyqtgraph example: ScatterPlotSpeedTest')
+    ui = Ui_MainWindow()
+    ui.setupUi(win)
+    win.show()
+
+    p = ui.plot
+    p.setLabel('left', 'Avg Module Voltage', units='V')
+    p.setLabel('bottom', 'Time', units='s')
+
+    QtGui.QApplication.instance().exec_()
+
+
+#def update():
+
 
 
 if __name__ == '__main__':
