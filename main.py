96fadc7ff8813ac82e760c5a77b1943fc4824a6e
AlexWang <alexn.wang@mail.utoronto.ca>
2016-04-27 21:57:50 -0400

    print to text function

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ import sys
 from PyQt4 import QtGui, QtCore
 from btiplottest_ui import Ui_MainWindow
 import bti
+import datetime #for text data outputs
 
 
 
-def main():
-
+def makeUI():
     app = QtGui.QApplication(sys.argv)
 
     win = QtGui.QMainWindow()
-    # win.setWindowTitle('pyqtgraph example: ScatterPlotSpeedTest')
+    win.setWindowTitle('pyqtgraph example: ScatterPlotSpeedTest')
     ui = Ui_MainWindow()
     ui.setupUi(win)
     win.show()
@@ -27,12 +27,21 @@ def main():
 
     QtGui.QApplication.instance().exec_()
 
-
 #def update():
     #dict = getlatestDict()
     #addnewpointstoplot()
 
-
+def fileOutput(inputDict):
+    '''
+    Prints the input dictionary out to a text file.
+    Current format is
+    YYYY-MM-DD HH:MM:SS || Key: Value | Key: Value ...
+    '''
+    outputFile = open("output.txt", "a")
+    outputFile.write("%s|| "%datetime.datetime.now())
+    for key in inputDict.keys():
+        outputFile.write("%s: %s | " %(key,inputDict[key]))
+    outputFile.write("\n")
 
 if __name__ == '__main__':
-    main()
+    makeUI()
