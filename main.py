e1860027d613bb5e99cbc684c5cf0ce9c262b951
Jerry Li <jerry@jerryli.ca>
2016-06-04 21:28:35 -0300

    starting to work on main GUI

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 '''
 Main BTI GUI code will be in this file
 '''
+
+import sys
+from PyQt4 import QtGui, QtCore, uic
+
+window_class = uic.loadUiType("main.ui")[0]
+
+class MainWindow(QtGui.QMainWindow, window_class):
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+        # set up plots and list views by looping through widget items
+        # button event handlers wil go here
+        
+    def start_listening():
+        pass
+    
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myWindow = MainWindow(None)
+    myWindow.show()
+    app.exec_()
